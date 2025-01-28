d2a3e701d68e0cefc53069bea79fc78f8988059e
Bobby Bloom <bloom@dev-and-design.de>
2025-01-28 11:57:06 +0100

    add view header and changed styles

--- app/src/main/java/com/kotbros/android_app/MainActivity.java
+++ app/src/main/java/com/kotbros/android_app/MainActivity.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
@@ -60,18 +62,21 @@ public class MainActivity extends AppCompatActivity implements NavigationBarView
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         final int id = item.getItemId();
         if (id == R.id.bmi_calc_menu) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.flFragment, bmiFragment)
-                    .commit();
+            setFrame(bmiFragment, R.string.bmi_calc_menu);
             return true;
         } else if (id == R.id.donation_menu) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.flFragment, donationFragment)
-                    .commit();
+            setFrame(donationFragment, R.string.donation_menu);
             return true;
         }
         return false;
+    }
+
+    private void setFrame(Fragment fragment, int resId) {
+        getSupportFragmentManager()
+            .beginTransaction()
+            .replace(R.id.flFragment, fragment)
+            .commit();
+        TextView header = (TextView)this.findViewById(R.id.fragment_header);
+        header.setText(getString(resId));
     }
 }
