c30c6853f69dfdaa1a66e17e3fa8b322fee502b0
Bobby Bloom <bloom@dev-and-design.de>
2025-01-28 22:35:21 +0100

    unfinished bmi ui refactoring

--- app/src/main/java/com/kotbros/android_app/BMIFragment.java
+++ app/src/main/java/com/kotbros/android_app/BMIFragment.java
@@ -1,34 +1,25 @@
 package com.kotbros.android_app;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.TextView;
-
-import com.kotbros.core.BMI;
 
 public class BMIFragment extends Fragment {
 
-    AutoCompleteTextView txt_height;
-    AutoCompleteTextView txt_weight;
+    public static final int MAX_HEIGHT = 350;
 
-    TextView txt_result_bmi;
-    TextView txt_result_cat;
+    private int height;
+    private TextView txtView_height;
 
-    public BMIFragment() {
-    }
+    private boolean isMale = true;
+
+    public BMIFragment() {}
 
     public static BMIFragment newInstance() {
         return new BMIFragment();
@@ -36,6 +27,7 @@ public class BMIFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        height = Integer.parseInt(getString(R.string.bmi_default_height));
         super.onCreate(savedInstanceState);
     }
 
@@ -43,62 +35,37 @@ public class BMIFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_bmi, container, false);
 
-        txt_height = view.findViewById(R.id.txt_height);
-        txt_weight = view.findViewById(R.id.txt_weight);
-        txt_result_bmi = view.findViewById(R.id.txt_result_bmi);
-        txt_result_cat = view.findViewById(R.id.txt_result_cat);
-        Button btn_more_info = view.findViewById(R.id.btn_more_info);
-
-        initTextField(txt_height);
-        initTextField(txt_weight);
-
-        btn_more_info.setOnClickListener(v -> {
-            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.wikipedia_bmi_link)));
-            startActivity(browserIntent);
-        });
+        txtView_height = view.findViewById(R.id.bmi_selection_height);
+        initHeightSlider(view);
 
         return view;
     }
 
-    private void initTextField(EditText editText) {
-        editText.addTextChangedListener(new TextWatcher() {
+    private void initHeightSlider(View view) {
+        SeekBar height_slider = view.findViewById(R.id.bmi_height_range_slider);
+
+        System.err.printf("height: %d%n", height);
+
+        height_slider.setMax(MAX_HEIGHT);
+        height_slider.setProgress(height, true);
+        height_slider.animate();
+
+        height_slider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                updateHeight(progress);
             }
 
             @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                calculateBmi();
-            }
+            public void onStartTrackingTouch(SeekBar seekBar) {}
 
             @Override
-            public void afterTextChanged(Editable editable) {
-            }
+            public void onStopTrackingTouch(SeekBar seekBar) {}
         });
     }
 
-    @SuppressLint("StringFormatMatches")
-    private void calculateBmi() {
-        if (isValidInput(txt_height) && isValidInput(txt_weight)) {
-            double bmi = BMI.calculate(getTextAsDouble(txt_height), getTextAsDouble(txt_weight));
-            txt_result_bmi.setText(getString(R.string.bmi_result, bmi));
-            txt_result_cat.setText(getString((BMI.category(bmi))));
-        } else {
-            txt_result_bmi.setText("");
-            txt_result_cat.setText("");
-        }
-    }
-
-    private boolean isValidInput(EditText editText) {
-        return getTextAsDouble(editText) > 0;
-    }
-
-    private double getTextAsDouble(EditText editText) {
-        String input = editText.getText().toString().replace(',', '.');
-        try {
-            return Double.parseDouble(input);
-        } catch (NumberFormatException e) {
-            return 0;
-        }
+    private void updateHeight(int height) {
+        this.height = height;
+        txtView_height.setText(String.valueOf(height));
     }
 }
