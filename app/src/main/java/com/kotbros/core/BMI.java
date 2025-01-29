1451f9cb1215a8551c4524dbd2cd47754e5197c5
Bobby Bloom <bloom@dev-and-design.de>
2025-01-29 15:06:31 +0100

    renamed bmi category string keys and modified bmi class

--- app/src/main/java/com/kotbros/core/BMI.java
+++ app/src/main/java/com/kotbros/core/BMI.java
@@ -4,41 +4,59 @@ import com.kotbros.android_app.R;
 
 public class BMI {
 
-    public static double calculate(double height, double weight) {
-        return Math.round(weight / Math.pow(height, 2) * 10d) / 10d;
+    public static double calculate(double weight, double height, int age) {
+        double bmi = weight / Math.pow(height, 2);
+        return Math.round(bmi * 10.0) / 10.0;
     }
 
-    public static int category(double bmi) {
+    public static int category(double bmi, int age) {
+        if (age < 17) {
+            return categorizeChildren(bmi);
+        }
+        return categorizeAdult(bmi);
+    }
+
+    private static int categorizeChildren(double bmi) {
         if (bmi < 15) {
-            return R.string.bmi_cat_1;
+            return R.string.bmi_category_underweight;
+        }
+        if (bmi < 22) {
+            return R.string.bmi_category_normal;
+        }
+        return R.string.bmi_category_overweight;
+    }
+
+    private static int categorizeAdult(double bmi) {
+        if (bmi < 15) {
+            return R.string.bmi_category_v_serv_underweight;
         }
         if (bmi < 16) {
-            return R.string.bmi_cat_2;
+            return R.string.bmi_category_serv_underweight;
         }
         if (bmi < 18.5) {
-            return R.string.bmi_cat_3;
+            return R.string.bmi_category_underweight;
         }
         if (bmi < 25) {
-            return R.string.bmi_cat_4;
+            return R.string.bmi_category_normal;
         }
         if (bmi < 30) {
-            return R.string.bmi_cat_5;
+            return R.string.bmi_category_overweight;
         }
         if (bmi < 35) {
-            return R.string.bmi_cat_6;
+            return R.string.bmi_category_obese1;
         }
         if (bmi < 40) {
-            return R.string.bmi_cat_7;
+            return R.string.bmi_category_obese2;
         }
         if (bmi < 45) {
-            return R.string.bmi_cat_8;
+            return R.string.bmi_category_obese3;
         }
         if (bmi < 50) {
-            return R.string.bmi_cat_9;
+            return R.string.bmi_category_obese4;
         }
         if (bmi < 60) {
-            return R.string.bmi_cat_10;
+            return R.string.bmi_category_obese5;
         }
-        return R.string.bmi_cat_11;
+        return R.string.bmi_category_obese6;
     }
 }
