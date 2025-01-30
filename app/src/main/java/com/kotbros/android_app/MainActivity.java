f04ecd6d9de7fca5fdb7fc6a69fc4becb46c8509
Bobby Bloom <bloom@dev-and-design.de>
2025-01-30 01:11:18 +0100

    modified fragment handling

--- app/src/main/java/com/kotbros/android_app/MainActivity.java
+++ app/src/main/java/com/kotbros/android_app/MainActivity.java
@@ -9,17 +9,24 @@ import android.view.MenuItem;
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationBarView.OnItemSelectedListener {
 
-    BottomNavigationView bottomNavigation;
-    BMIFragment bmiFragment;
-    DonationFragment donationFragment;
+    private BottomNavigationView bottomNavigation;
+    private BMIFragment bmiFragment;
+    private DonationFragment donationFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +39,22 @@ public class MainActivity extends AppCompatActivity
 
         bottomNavigation = findViewById(R.id.bottomNavigationView);
         bottomNavigation.setOnItemSelectedListener(this);
-        bottomNavigation.setSelectedItemId(R.id.bmi_calc_menu);
         bottomNavigation.setBackgroundColor(0);
+        bottomNavigation.setSelectedItemId(R.id.bmi_calc_menu);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_activity_container), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        addBackStackChangeListener();
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        getOnBackPressedDispatcher().onBackPressed();
+        return true;
     }
 
     @Override
@@ -46,7 +67,10 @@ public class MainActivity extends AppCompatActivity
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.action_about) {
+        if (id == android.R.id.home) {
+            getOnBackPressedDispatcher().onBackPressed();
+            return true;
+        } else if (id == R.id.action_about) {
             Intent i = new Intent(this, AboutActivity.class);
             startActivity(i);
             return true;
@@ -60,13 +84,15 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(false);
         final int id = item.getItemId();
         if (id == R.id.bmi_calc_menu) {
-            setFrame(bmiFragment);
+            showFragment(bmiFragment);
             return true;
         } else if (id == R.id.donation_menu) {
-            setFrame(donationFragment);
+            showFragment(donationFragment);
             playDonationSound();
             return true;
         }
@@ -74,15 +100,35 @@ public class MainActivity extends AppCompatActivity
         return false;
     }
 
+    private void addBackStackChangeListener() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.addOnBackStackChangedListener(() -> {
+            if (getSupportActionBar() != null) {
+                boolean showBackButton = fragmentManager.getBackStackEntryCount() > 0;
+                getSupportActionBar().setDisplayHomeAsUpEnabled(showBackButton);
+            }
+
+            updateBottomNavigation(fragmentManager.findFragmentById(R.id.flFragment));
+        });
+    }
+
+    private void updateBottomNavigation(Fragment fragment) {
+        if (fragment instanceof BMIFragment) {
+            bottomNavigation.setSelectedItemId(R.id.bmi_calc_menu);
+        } else if (fragment instanceof DonationFragment) {
+            bottomNavigation.setSelectedItemId(R.id.donation_menu);
+        }
+    }
+
+    private void showFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction()
+                .replace(R.id.flFragment, fragment);
+
+        transaction.commit();
+    }
+
     private void playDonationSound() {
         MediaPlayer mp = MediaPlayer.create(this, R.raw.donation_please);
         mp.start();
-    }
-
-    private void setFrame(Fragment fragment) {
-        getSupportFragmentManager()
-            .beginTransaction()
-            .replace(R.id.flFragment, fragment)
-            .commit();
     }
 }
